edd14a9d3ac5fb26cdc35f9840d2a396f64eca89
browning <browning@uw.edu>
2022-05-20 17:56:50 -0700

    Add version number.

--- src/ibdends/IbdEndsMain.java
+++ src/ibdends/IbdEndsMain.java
@@ -47,7 +47,7 @@ import java.util.concurrent.Executors;
 public class IbdEndsMain {
 
     private static final String EXECUTABLE = "ibd-ends.jar";
-    private static final String PROGRAM = EXECUTABLE + "  [version 1.1, __REV__]";
+    private static final String PROGRAM = EXECUTABLE + "  [version 1.1, 20May22.6cb]";
     private static final String COPYRIGHT = "Copyright (C) 2020 Brian L. Browning";
 
     /**
